ab36d8c7d26482348368218f781ee3fefee18a35
ibkonate <djbrino@gmail.com>
2022-08-23 18:29:21 +0000

    Add of table domicile

--- db/noyau.sql
+++ db/noyau.sql
@@ -8,4 +8,10 @@ CREATE TABLE IF NOT EXISTS adresse(
     id INT AUTOINCREMENT PRIMARY KEY,
     depuis DATE,
     
+CREATE TABLE IF NOT EXISTS domicile(
+    quartier VARCHAR PRIMARY KEY,
+    rue VARCHAR(20),
+    porte VARCHAR(30),
+    localite VARCHAR(20);
+)
 )
